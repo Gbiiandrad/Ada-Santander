87a004abba3a13fd67a13d2336cb8da9bfd0b5dd
Gabrielle Andrade Da Silva <105249937+Gbiiandrad@users.noreply.github.com>
2023-05-27 17:42:50 -0300

    Usando o If e Else

---
+++ CursPOO/src/Main.java
@@ -0,0 +1,20 @@
+import Animais.Cachorro;
+
+public class Main {
+    public static void main(String[] args) {
+        Cachorro cachorro1 = new Cachorro();
+        cachorro1.nome = "Puppy";
+        cachorro1.cor = "marron";
+        cachorro1.altura = 25;
+        cachorro1.peso = 5.5;
+        cachorro1.tamanhoDorabo = 5;
+
+        cachorro1.latir();
+        System.out.println("O cachorro pegou uma.." + cachorro1.pegar());
+
+        System.out.println("O cachorro está " + cachorro1.interagir("carinho"));
+        System.out.println("O cachorro está " + cachorro1.interagir("dormir"));
+        System.out.println("O cachorro está " + cachorro1.interagir("nada"));
+
+    }
+}
